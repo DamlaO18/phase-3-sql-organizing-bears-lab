cb5346559e2e92a6887c7bccddcbbe47086da82a
Damla Osman <damlaaosman@gmail.com>
2022-09-28 21:34:44 +1000

    commit tree b55b3aea1ce789177606f8daf56fae44580ec1ab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, sex, color) VALUES ('Mr. Chocolate', 'M', 'Brown');
+INSERT INTO bears (name, sex, color) VALUES ('Rowdy', 'M', 'Brown');
+INSERT INTO bears (name, sex, color) VALUES ('Tabitha', 'F', 'Pink');
+INSERT INTO bears (name, sex, color) VALUES ('Sergeant Brown', 'M', 'Blue');
+INSERT INTO bears (name, sex, color) VALUES ('Melissa', 'F', 'Pink');
+INSERT INTO bears (name, sex, color) VALUES ('Grinch', 'M', 'Green');
+INSERT INTO bears (name, sex, color) VALUES ('Wendy', 'F', 'Purple');
+INSERT INTO bears (sex, color) VALUES ('F', 'Purple');
